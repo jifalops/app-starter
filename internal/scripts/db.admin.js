e35788ee0b0b8d5e8e0a60f2d88141b8489a2d2b
Jacob Phillips <jifalops@gmail.com>
2017-02-05 14:43:10 -0500

    Moved eventually private stuff to internal/. Added more rules, images, and using the images in index.html. Also added behaviors that need to use the polymer 2.0 ways.

---
+++ internal/scripts/db.admin.js
@@ -0,0 +1,20 @@
+var db = db || {};
+
+db.admin = {
+  becomeRoot: function(user, onSuccess, onFailure) {
+    var updates = db.userActionUpdate(user);
+    updates['/roles/hasRoot'] = true;
+    updates['/roles/root'] = user;
+    updates['/roles/superAdmins/'   + user] = db.timestamp();
+    updates['/roles/admins/'        + user] = db.timestamp();
+    updates['/roles/maintainers/'   + user] = db.timestamp();
+    updates['/roles/moderators/'    + user] = db.timestamp();
+    updates['/users/' + user + '/isRoot']        = true;
+    updates['/users/' + user + '/isSuperAdmin']  = true;
+    updates['/users/' + user + '/isAdmin']       = true;
+    updates['/users/' + user + '/isMaintainer']  = true;
+    updates['/users/' + user + '/isModerator']   = true;
+    updates['/users/' + user + '/isElevated']    = true;
+    db.update(updates, onSuccess, onFailure);
+  }
+};
