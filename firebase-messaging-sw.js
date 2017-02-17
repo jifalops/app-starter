c0d9e1c090a93424a12ecd643d2fbc74a2682c60
Jacob Phillips <jifalops@gmail.com>
2017-02-16 21:22:48 -0500

    testing push notifications.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -21,17 +21,17 @@ const messaging = firebase.messaging();
 // implement this optional method.
 // [START background_handler]
 
-// messaging.setBackgroundMessageHandler(function(payload) {
-//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//   // Customize notification here
-//   const notificationTitle = 'Background Message Title';
-//   const notificationOptions = {
-//     body: 'Background Message body.',
-//     icon: '/firebase-logo.png'
-//   };
-//
-//   return self.registration.showNotification(notificationTitle,
-//       notificationOptions);
-// });
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/images/app-icon-32.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
 
 // [END background_handler]
