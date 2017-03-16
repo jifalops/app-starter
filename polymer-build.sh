2643b6a4e02afad4ba602e5f6ec44a16a716c6c0
Jacob Phillips <jifalops@gmail.com>
2017-03-16 15:23:06 -0400

    Build does not include service worker

--- polymer-build.sh
+++ polymer-build.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-# "shell" must be a relative path in polymer.json.            src/app-shell.html
-# "fragments" must use an absolute path in polymer.json.      /src/frag-mnt.html
-# HTML imports must use relative paths inside the "shell" file.
-# Tested with polymer-cli 0.17.0 and polymer 1.7.1.
-polymer build
-cp src/app-shell.html build/bundled/src/    # copy app shell
-# cp manifest.json build/bundled/             # copy manifest
-# cp firebase-messaging-sw.js build/bundled/  # copy FCM service worker
-cp -rn bower_components build/bundled/      # copy only missing bower components
-# Minify html (npm i -g html-minifier)
-# html-minifier --html5 --input-dir build/bundled/ --output-dir build/bundled --file-ext html --minify-css --minify-js --minify-ur-ls
-cd internal/
-./make-rules
-cd ..
+polymer build --sw-precache-config sw-precache-config.js
+cp -n src/app-shell.html build/bundled/src/    # copy app shell if necessary
+cp -rn bower_components build/bundled/         # copy any missing bower components
+# Make rules.json
+cd internal
+./make-rules.sh
