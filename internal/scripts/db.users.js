bc7a44db8cec9e4d26ac448459469ea184ca09c9
Jacob Phillips <jifalops@gmail.com>
2017-03-16 15:48:41 -0400

    Added service worker dependency and fix log messages

--- internal/scripts/db.users.js
+++ internal/scripts/db.users.js
@@ -2,7 +2,7 @@ var db = db || {};
 
 db.users = {
   createUser: function(uid, username, user, profile, onSuccess, onFailure) {
-    DB && console.log('Creating user ' + username);
+    LOG && DB && console.log('Creating user ' + username);
     var updates = {};
     updates['/uids/' + uid] = username;
     updates['/usernames/' + username.toLowerCase()] = true;
@@ -12,14 +12,14 @@ db.users = {
   },
 
   changeAvatar: function (user, url, onSuccess, onFailure) {
-    DB && console.log('Changing profile pic to ' + url);
+    LOG && DB && console.log('Changing profile pic to ' + url);
     var updates = db.userActionUpdate(user);
     updates['/profiles/' + user + '/pic'] = url;
     db.update(updates, onSuccess, onFailure);
   },
 
   updateLocation: function(user, lat, lng, onSuccess, onFailure) {
-    DB && console.log('Updating location to', lat, lng);
+    LOG && DB && console.log('Updating location to', lat, lng);
     var geofire = new GeoFire(db.ref('/geoUsers'));
     geofire.set(user, [lat, lng]).then(function () {
       if (onSuccess) onSuccess();
@@ -29,14 +29,14 @@ db.users = {
   },
 
   changeDisplayName: function (user, name, onSuccess, onFailure) {
-    DB && console.log('Changing display name to ' + name);
+    LOG && DB && console.log('Changing display name to ' + name);
     var updates = db.userActionUpdate(user);
     updates['/profiles/' + user + '/name'] = name;
     db.update(updates, onSuccess, onFailure);
   },
 
   setElevated: function(user, elevated, onSuccess, onFailure) {
-    DB && console.log('Setting elevated to ' + elevated);
+    LOG && DB && console.log('Setting elevated to ' + elevated);
     var updates = db.userActionUpdate(user);
     updates['/users/' + user + '/isElevated'] = elevated;
     db.update(updates, onSuccess, onFailure);
@@ -44,7 +44,7 @@ db.users = {
 
   sendFeedback: function(user, feedback, onSuccess, onFailure) {
     var key = db.newKey('/feedback/');
-    DB && console.log('Sending feedback', key);
+    LOG && DB && console.log('Sending feedback', key);
     var updates = db.userActionUpdate(user);
     updates['/feedback/' + key] = {
       text: feedback,
@@ -55,7 +55,7 @@ db.users = {
   },
 
   sendMessage: function(user, otherUsers, chat, msg, onSuccess, onFailure) {
-    DB && console.log('Sending message to', otherUsers);
+    LOG && DB && console.log('Sending message to', otherUsers);
     var updates = db.userActionUpdate(user);
 
     if (!chat) {
