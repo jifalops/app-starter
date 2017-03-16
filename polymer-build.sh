7fec43f9da50134398639225bdd636b5dc121bd0
Jacob Phillips <jifalops@gmail.com>
2017-03-16 16:00:51 -0400

    fuck service workers

--- polymer-build.sh
+++ polymer-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-polymer build --sw-precache-config sw-precache-config.js
+polymer build
 cp -n src/app-shell.html build/bundled/src/    # copy app shell if necessary
 cp -rn bower_components build/bundled/         # copy any missing bower components
 # Make rules.json
