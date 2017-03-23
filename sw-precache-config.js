2acb7ff779b2f9362c61ab5c074923ea8b53cfe3
Jacob Phillips <jifalops@gmail.com>
2017-03-23 13:46:40 -0400

    Fix sw redirect for auth popup

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -4,7 +4,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/*',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
   ],
   navigateFallback: 'index.html',
+  navigateFallbackWhitelist: [/^(?!\/__)/]
 };
